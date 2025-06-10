6117795b9ddca55f23b5cc172e3eda1c07a701c7
alliixx <allison.nmn.xin@gmail.com>
2025-06-09 20:36:12 -0700

    get rid of key

--- parallel_prompt_testing/run_experiment.py
+++ parallel_prompt_testing/run_experiment.py
@@ -8,8 +8,9 @@ from langchain.schema import SystemMessage, HumanMessage, AIMessage
 from langchain.chat_models import ChatOpenAI
 from contextlib import redirect_stdout
 import time
+import os
 
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 DRY_RUN = False
 request_counter = {"llm": 0}
 
